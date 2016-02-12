f8a4b2e951faa44b1194202f4dcab39e5ca9ca22
Ming Chen <ming8924@gmail.com>
2016-02-11 18:52:45 -0800

    batch mode

--- pipa_load_im.m
+++
@@ -1,9 +0,0 @@
-function [im, bbox] = pipa_load_im(index, split_set, data)
-% return an image specified by the index and split set
-% Note: index and split_set are actually redundant
-%   global data;
-  im_dir = sprintf('../data/pipa/%s', split_set);
-  im_name = sprintf('%s/%s_%s.jpg',im_dir, data.photoset_ids{index}, data.photo_ids{index});
-  bbox = data.head_boxes(index, :);
-  im = imread(im_name);
-end
